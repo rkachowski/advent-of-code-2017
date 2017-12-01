ca9be94659ef3f5e3f1a8ba3b5d2dc3b26d285c3
Donald Hutchison <git@toastymofo.net>
2017-12-01 12:06:53 +0100

    Remove crap.

--- 1/solution.rb
+++ 1/solution.rb
@@ -1,11 +1,8 @@
 input = File.read("input").strip
 
-input.to_enum(:scan, /(\d)\1+/).map {Regexp.last_match }
-
 #+ 6 because of list wrapping and i'm lazy
 part1 = input.to_enum(:scan, /(\d)\1+/).map {Regexp.last_match[1].to_i * (Regexp.last_match[0].size() -1) }.sum + 6
+part2 = input.chars.select.with_index {|c,i|  c == input[i + input.size/2] }.map{|c| c.to_i * 2}.sum
 
 puts "part 1 #{part1}"
-
-part2 = input.chars.select.with_index {|c,i|  c == input[i + input.size/2] }.map{|c| c.to_i * 2}.sum
 puts "part 2 #{part2}"
