1c93ac4dae7bafa976ee4d583c45f379d804978c
Donald Hutchison <git@toastymofo.net>
2017-12-20 20:07:28 +0100

    day 20 part 2.

--- 20/solution.rb
+++ 20/solution.rb
@@ -7,21 +7,16 @@ end
 
 add = ->(ar, ar2) { ar[0] += ar2[0] ; ar[1] += ar2[1] ; ar[2] += ar2[2] }
 tick = ->(p){ add.call(p[1],p[2]); add.call(p[0],p[1]) }
-acc = ->(a,t,v,p){ 0.5 * a * t**2 + v*t + p}
-update = ->(p, t, i){ acc.call(p[2][i],t, p[1][i], p[0][i])  }
-=begin
-time = 99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
-positions = input.map do |p|
-  [update.call(p,time,0),update.call(p,time,1),update.call(p,time,2)]
-end
-
-
-distances = positions.map {|p| p.sum.abs  }
-puts distances.index(distances.min)
-
-
-=end
 
 mags = input.map { |p| Math.sqrt(p[2][0]**2 + p[2][1]**2 + p[2][2]**2) }
-
 puts mags.index(mags.min)
+
+20000.times do |i|
+  positions = Hash.new {|hsh,k| hsh[k] = []}
+  input.each { |p| tick.call(p); positions[p[0].to_s] << p }
+
+  to_delete = positions.values.select { |v| v.size > 1 }
+  to_delete.each {|ps| ps.each {|p|input.delete(p)}}
+end
+
+puts input.size
