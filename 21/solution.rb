e3ad1fc1b7e8ecea618c0a3d9a63656373c64a95
Donald Hutchison <git@toastymofo.net>
2018-01-09 11:57:11 +0100

    day 21 whoa that was insane

--- 21/solution.rb
+++ 21/solution.rb
@@ -1,11 +1,16 @@
+require 'json'
+
 @pattern = <<~PATTERN
-    .#..#.
-    ..#..#
-    ######
-    .#..#.
-    ..#..#
-    ######
+    .#.
+    ..#
+    ###
 PATTERN
+
+def clone_pat pat
+  JSON.parse(JSON.generate(pat))
+end
+
+@pattern = @pattern.lines.map(&:chomp).map(&:chars)
 
 def flip_x pat
   pat.dup.each { |l| last = l.size - 1;  l[0], l[last]  = l[last],l[0]}
@@ -27,17 +32,25 @@ def pat arr
   arr.each {|l| puts l.join}
 end
 
-def extract_segments size, pats
-  p = pats.each_slice(size).map{|l| l.flatten.each_slice(size).to_a }
-  even_segs = p.map {|l| l.select.with_index {|_,i| i.even? }.flatten}
-  odd_segs = p.map {|l| l.select.with_index {|_,i| i.odd? }.flatten}
-  even_segs.zip( odd_segs).flatten(1)
+def extract_segments pats
+  split_size = pats.first.size % 2 == 0 ? 2 : 3
+
+  segs = []
+
+  pats.each_with_index do |row,y|
+    row.each_with_index do |_,x|
+      position = (((y/split_size).floor * row.size + x) / split_size).floor
+      segs[position] ||=[]
+      segs[position] << pats[y][x]
+    end
+  end
+
+  segs.map{|s| s.each_slice(split_size).to_a}
 end
 
+#create a tree of rules
 @rules = Hash.new {|hash, k| hash[k] = {} }
 input = File.read("input").lines
-input = "../.# => ##./#../...
-.#./..#/### => #..#/..../..../#..#".lines
 input.map! {|l| l.split(" => ").map(&:strip)}
 
 input.each do |inp, outp|
@@ -52,19 +65,40 @@ input.each do |inp, outp|
   end
 end
 
-def iterate
-  #assume square
-  pat @pattern
-  split_size = @pattern.first.size % 2 ? 2 : 3
-  if @pattern.first.size % 2
-    #split into twos
-    @pattern.each_slice(2).to_a
-  else
-    #split into threes
-  end
+@rules = clone_pat @rules
 
-  #transform each sub pattern
-  #join
+def iterate
+  squares = extract_segments @pattern
+
+  output = squares.map { |s| find_rule s, @rules }
+  sq_size = Math.sqrt(output.size).to_i
+  o = output.each_slice(sq_size).to_a
+
+  o.map!{|s|s.map{|f| f.split("/")}}
+  output = o.map {|s| s[0].zip(*s[1..-1])}.flatten(1).map {|s| s.map(&:chars).flatten(1) }
+  puts output.flatten.count("#")
+  output
 end
 
+def permute sq
+  p = clone_pat sq
+
+  return enum_for(:permute, p) unless block_given?
+
+  [p, flip_x(p), flip_y(p), flip_x(flip_y(p))].lazy.each do |v|
+    yield v
+    yield rotate(v)
+    yield rotate(rotate(v))
+    yield rotate(rotate(rotate(v)))
+  end
+end
+
+def find_rule seq, rules
+  permute(seq).each { |p|
+    o = p.map(&:join)
+   return rules.dig(*o) if rules.dig(*o)}
+
+  puts "failed to find rule for #{seq.to_s}"
+  nil
+end
 
